197ae633132cee1c7c9ffc9349a30412f03d21f7
Michael 'PoempelFox' Meier <git@mulder.franken.de>
2018-03-07 22:53:08 +0100

    this could theoretically work now - sadly, it doesn't.

--- lufa/console.c
+++ lufa/console.c
@@ -1,3 +1,11 @@
+/* This originally was the "VirtualSerial" example from the LUFA library.
+ * It has been turned into a proper serial console for foxgeig2018.
+ * What follows is the original copyright notice, although probably not enough
+ * of the old example remains to warrant that.
+ */
+
+#if (defined(SERIALCONSOLE))
+
 /*
              LUFA Library
      Copyright (C) Dean Camera, 2017.
@@ -28,13 +36,46 @@
   this software.
 */
 
-/* This originally was the "VirtualSerial" example from the LUFA library.
- * It has been turned into a proper serial console for foxgeig2018.
- */
+
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+#include <avr/interrupt.h>
+#include <string.h>
 
 #include "console.h"
+#include "Descriptors.h"
+#include <LUFA/Drivers/USB/USB.h>
 
-/** Contains the current baud rate and other settings of the virtual serial port. While this demo does not use
+
+#define INPUTBUFSIZE 30
+static uint8_t inputbuf[INPUTBUFSIZE];
+static uint8_t inputpos = 0;
+#define OUTPUTBUFSIZE 400
+static uint8_t outputbuf[OUTPUTBUFSIZE];
+static uint16_t outputhead = 0; /* WARNING cannot be modified atomically */
+static uint16_t outputtail = 0;
+static uint8_t escstatus = 0;
+static const uint8_t CRLF[] PROGMEM = "\r\n";
+static const uint8_t WELCOMEMSG[] PROGMEM = "\r\n"\
+                                   "\r\n ***************"\
+                                   "\r\n * foxgeig2018 *"\
+                                   "\r\n ***************"\
+                                   "\r\n"\
+                                   "\r\nAVR-libc: " __AVR_LIBC_VERSION_STRING__ " (" __AVR_LIBC_DATE_STRING__ ")"\
+                                   "\r\nSoftware Version 0.1, Compiled " __DATE__ " " __TIME__;
+static const uint8_t PROMPT[] PROGMEM = "\r\n# ";
+
+/* external variables */
+/* these are defined in main.c and contain our last measured data for output
+ * in the status command.
+ * Note: Since we execute from interrupt context, we might occasionally
+ * get corrupted values in the uint16_ts.
+ */
+/* FIXME no external variables yet */
+
+/* Contains the current baud rate and other settings of the virtual serial port. While this demo does not use
  *  the physical USART and thus does not use these settings, they must still be retained and returned to the host
  *  upon request or the host will assume the device is non-functional.
  *
@@ -48,34 +89,22 @@ static CDC_LineEncoding_t LineEncoding = { .BaudRateBPS = 0,
                                            .DataBits    = 8                            };
 
 
-/** Main program entry point. This routine contains the overall program flow, including initial
- *  setup of all components and the main program loop.
- */
-void console_init(void)
-{
-	USB_Init();
-}
-
-
-void console_work(void)
-{
-	CDC_Task();
-}
-
-/** Event handler for the USB_Connect event. This indicates that the device is enumerating via the status LEDs and
- *  starts the library USB task to begin the enumeration and USB management process.
+/* Event handler for the USB_Connect event. This indicates that the device is enumerating
  */
 void EVENT_USB_Device_Connect(void)
 {
-	/* Indicate USB enumerating */
+  /* Nothing to do? */
 }
 
-/** Event handler for the USB_Disconnect event. This indicates that the device is no longer connected to a host via
- *  the status LEDs and stops the USB management and CDC management tasks.
+/* Event handler for the USB_Disconnect event. This indicates that the device is
+ * no longer connected to a host
  */
 void EVENT_USB_Device_Disconnect(void)
 {
-	/* Indicate USB not ready */
+  /* Throw away all our buffers. */
+  inputpos = 0;
+  outputhead = 0;
+  outputtail = 0;
 }
 
 /** Event handler for the USB_ConfigurationChanged event. This is fired when the host set the current configuration
@@ -141,11 +170,181 @@ void EVENT_USB_Device_ControlRequest(void)
 	}
 }
 
-/** Function to manage CDC data transmission and reception to and from the host. */
+#if defined __GNUC__
+static void appendchar(uint8_t what) __attribute__((noinline));
+#endif /* __GNUC__ */
+/* This can only be called safely with interrupts disabled - remember that! */
+static void appendchar(uint8_t what) {
+  uint16_t newpos;
+  newpos = (outputtail + 1);
+  if (newpos >= OUTPUTBUFSIZE) {
+    newpos = 0;
+  }
+  if (newpos != outputhead) {
+    outputbuf[outputtail] = what;
+    outputtail = newpos;
+  }
+}
+
+/* We do all query processing here.
+ * This must be called with IRQs disabled.
+ */
+static void console_inputchar(uint8_t inpb) {
+  if (escstatus == 1) {
+    if (inpb == '[') {
+      escstatus = 2;
+    } else {
+      escstatus = 0;
+      appendchar(7); /* Bell */
+    }
+    return;
+  }
+  if (escstatus == 2) {
+    switch (inpb) {
+    case 'A': /* Up */
+            /* Try to restore the last comnmand */
+            for (; inputpos > 0; inputpos--) {
+              appendchar(8);
+            }
+            while (inputbuf[inputpos]) {
+              appendchar(inputbuf[inputpos++]);
+            }
+            break;
+    case 'B': /* Down */
+            /* New empty command line */
+            for (; inputpos > 0; inputpos--) {
+              appendchar(8);
+              appendchar(' ');
+              appendchar(8);
+            }
+            break;
+    case 'C': /* Left */
+    case 'D': /* Right */
+    default:
+            appendchar(7); /* Bell */
+            break;
+    };
+    escstatus = 0;
+    return;
+  }
+  /* escstatus == 0, i.e. not in an escape */
+  switch (inpb) {
+  case 0 ... 7: /* Nonprinting characters. Ignore. */
+  case 11 ... 12: /* Nonprinting characters. Ignore. */
+  case 14 ... 26: /* Nonprinting characters. Ignore. */
+  case 28 ... 31: /* Nonprinting characters. Ignore. */
+  case 0x7f ... 0xff: /* Nonprinting characters. Ignore. */
+          console_printhex8_noirq(inpb);
+          appendchar(7); /* Bell */
+          break;
+  case 9: /* tab. Should be implemented some day? */
+          appendchar(7); /* Bell */
+          break;
+  case 27: /* Escape */
+          escstatus = 1;
+          break;
+  case 8: /* Backspace */
+          if (inputpos > 0) {
+            inputpos--;
+            appendchar(inpb);
+            appendchar(' ');
+            appendchar(inpb);
+          }
+          break;
+  case '\r': /* 13 */
+  case '\n': /* 10 */
+          if (inputpos == 0) {
+            console_printpgm_noirq_P(PROMPT);
+            break;
+          }
+          inputbuf[inputpos] = 0; /* Null-terminate the string */
+          console_printpgm_noirq_P(CRLF);
+          /* now lets see what it is */
+          if        (strcmp_P(inputbuf, PSTR("help")) == 0) {
+            console_printpgm_noirq_P(PSTR("Available commands:"));
+            console_printpgm_noirq_P(PSTR("\r\n motd             repeat welcome message"));
+            console_printpgm_noirq_P(PSTR("\r\n showpins [x]     shows the avrs inputpins"));
+            console_printpgm_noirq_P(PSTR("\r\n status           show status / counters"));
+          } else if (strcmp_P(inputbuf, PSTR("motd")) == 0) {
+            console_printpgm_noirq_P(WELCOMEMSG);
+          } else if (strncmp_P(inputbuf, PSTR("showpins"), 8) == 0) {
+            uint8_t which = 0;
+            uint8_t stal = 1; uint8_t endl = 4;
+            if (inputpos == 10) {
+                    switch (inputbuf[9]) {
+                    case 'a':
+                    case 'A':
+                            which = 1; break;
+                    case 'b':
+                    case 'B':
+                            which = 2; break;
+                    case 'c':
+                    case 'C':
+                            which = 3; break;
+                    case 'd':
+                    case 'D':
+                            which = 4; break;
+                    case 'e':
+                    case 'E':
+                            which = 5; break;
+                    };
+            }
+            if (which) {
+                    stal = which;
+                    endl = which;
+            }
+            for (which = stal; which <= endl; which++) {
+              uint8_t pstatus;
+              switch (which) {
+              case 2: pstatus = PINB;
+                      console_printpgm_noirq_P(PSTR("PINB: 0x"));
+                      break;
+              case 3: pstatus = PINC;
+                      console_printpgm_noirq_P(PSTR("PINC: 0x"));
+                      break;
+              case 4: pstatus = PIND;
+                      console_printpgm_noirq_P(PSTR("PIND: 0x"));
+                      break;
+              case 5: pstatus = PINE;
+                      console_printpgm_noirq_P(PSTR("PINE: 0x"));
+                      break;
+              default:
+                      pstatus = 0;
+                      console_printpgm_noirq_P(PSTR("NOPIN: 0x"));
+                      break;
+              };
+              console_printhex8_noirq(pstatus);
+              appendchar(' ');
+              console_printbin8_noirq(pstatus);
+              if (which < endl) {
+                appendchar('\r'); appendchar('\n');
+              }
+            }
+          } else if (strcmp_P(inputbuf, PSTR("status")) == 0) {
+            console_printpgm_noirq_P(PSTR("No status yet, sorry\r\n"));
+          } else {
+            console_printpgm_noirq_P(PSTR("Unknown command: "));
+            console_printtext_noirq(inputbuf);
+          }
+          /* show PROMPT and go back to start. */
+          console_printpgm_noirq_P(PROMPT);
+          inputpos = 0;
+          break;
+  default:
+          if (inputpos < (INPUTBUFSIZE - 1)) { /* -1 for terminating \0 */
+                  inputbuf[inputpos++] = inpb;
+                  /* Echo the character */
+                  appendchar(inpb);
+          } else {
+                  appendchar(7); /* Bell */
+          }
+  };
+}
+
+/* Function to manage CDC data transmission and reception to and from the host. */
+/* call with interrupts disabled! */
 void CDC_Task(void)
 {
-  char prbuf[100];
-
   /* Device must be connected and configured for the task to run */
   if (USB_DeviceState != DEVICE_STATE_Configured)
     return;
@@ -153,40 +352,161 @@ void CDC_Task(void)
   /* Select the Serial Rx Endpoint */
   Endpoint_SelectEndpoint(CDC_RX_EPADDR);
 
-  /* Throw away any received data from the host */
-  if (Endpoint_IsOUTReceived()) {
+  if (Endpoint_IsOUTReceived()) { /* Do we have output to read? Then read it. */
     uint8_t errorcode;
     uint8_t inp[2];
-    errorcode = Endpoint_Read_Stream_LE(inp, 1, NULL);
-    if (errorcode != ENDPOINT_RWSTREAM_NoError) {
-      return;
+    uint8_t i;
+    uint8_t bytestoread = Endpoint_BytesInEndpoint();
+    for (i = 0; i < bytestoread; i++) {
+      errorcode = Endpoint_Read_Stream_LE(inp, 1, NULL);
+      if (errorcode != ENDPOINT_RWSTREAM_NoError) {
+        break;
+      }
+      console_inputchar(inp[0]);
     }
-    
+    /* Endpoint_ClearOUT(); */
+  }
+
+  /* Do we have anything to send, and can we send? */
+  if ((outputhead != outputtail) && (Endpoint_IsINReady())) {
+    uint8_t whattosend[CDC_TXRX_EPSIZE];
+    uint8_t bytestosend = 0;
+    /* Note: We send one byte less than the maximum on purpose, because if the
+     * host would receive a maximum sized packet, it would wait for more to
+     * follow. */
+    while ((bytestosend < (CDC_TXRX_EPSIZE - 1)) && (outputhead != outputtail)) {
+      whattosend[bytestosend] = outputbuf[outputhead];
+      outputhead++;
+      if (outputhead >= OUTPUTBUFSIZE) {
+        outputhead = 0;
+      }
+      bytestosend++;
+    }
     /* Select the Serial Tx Endpoint */
     Endpoint_SelectEndpoint(CDC_TX_EPADDR);
-    sprintf(prbuf, "Received: %c\n", inp[0]);
-    Endpoint_Write_Stream_LE(prbuf, strlen(prbuf), NULL);
+    Endpoint_Write_Stream_LE(whattosend, bytestosend, NULL);
     Endpoint_ClearIN();
   }
 
-#if 0
-          /* Remember if the packet to send completely fills the endpoint */
-          bool IsFull = (Endpoint_BytesInEndpoint() == CDC_TXRX_EPSIZE);
-
-          /* Finalize the stream transfer to send the last packet */
-          Endpoint_ClearIN();
-
-          /* If the last packet filled the endpoint, send an empty packet to release the buffer on
-           * the receiver (otherwise all data will be cached until a non-full packet is received) */
-          if (IsFull)
-          {
-                  /* Wait until the endpoint is ready for another packet */
-                  Endpoint_WaitUntilReady();
-
-                  /* Send an empty packet to ensure that the host does not buffer data sent to it */
-                  Endpoint_ClearIN();
-          }
-  }
-#endif
-
 }
+
+void console_printchar_noirq(uint8_t what) {
+  appendchar(what);
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+void console_printhex8_noirq(uint8_t what) {
+  uint8_t buf;
+  uint8_t i;
+  for (i=0; i<2; i++) {
+    buf = (uint8_t)(what & (uint8_t)0xF0) >> 4;
+    if (buf <= 9) {
+      buf += '0';
+    } else {
+      buf += 'A' - 10;
+    }
+    appendchar(buf);
+    what <<= 4;
+  }
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+void console_printdec_noirq(uint8_t what) {
+  uint8_t buf;
+  buf = what / 100;
+  appendchar(buf + '0');
+  what %= 100;
+  buf = what / 10;
+  appendchar(buf + '0');
+  buf = what % 10;
+  appendchar(buf + '0');
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+/* This is the same as printec, but only prints 2 digits (e.g. for times/dates) */
+void console_printdec2_noirq(uint8_t what) {
+  if (what > 99) { what = 99; }
+  appendchar((what / 10) + '0');
+  appendchar((what % 10) + '0');
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+void console_printbin8_noirq(uint8_t what) {
+  uint8_t i;
+  for (i = 0; i < 8; i++) {
+    if (what & 0x80) {
+      appendchar('1');
+    } else {
+      appendchar('0');
+    }
+    what <<= 1;
+  }
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+void console_printtext_noirq(const uint8_t * what) {
+  while (*what) {
+    appendchar(*what);
+    what++;
+  }
+}
+
+/* This can only be called safely with interrupts disabled - remember that! */
+void console_printpgm_noirq_P(PGM_P what) {
+  uint8_t t;
+  while ((t = pgm_read_byte(what++))) {
+    appendchar(t);
+  }
+}
+
+/* These are wrappers for our internal functions, disabling IRQs before
+ * calling them. */
+void console_printtext(const uint8_t * what) {
+  cli();
+  console_printtext_noirq(what);
+  sei();
+}
+
+void console_printpgm_P(PGM_P what) {
+  cli();
+  console_printpgm_noirq_P(what);
+  sei();
+}
+
+void console_printhex8(uint8_t what) {
+  cli();
+  console_printhex8_noirq(what);
+  sei();
+}
+
+void console_printdec(uint8_t what) {
+  cli();
+  console_printdec_noirq(what);
+  sei();
+}
+
+/* Initialize ourselves. Must be called with interrupts still disabled! */
+void console_init(void)
+{
+  USB_Init();
+  console_printpgm_noirq_P(WELCOMEMSG);
+  console_printpgm_noirq_P(PROMPT);
+}
+
+void console_work(void)
+{
+  cli();
+  CDC_Task();
+  sei();
+}
+
+#else /* SERIALCONSOLE */
+
+void console_init(void) { }
+void console_printchar_noirq(uint8_t c) { }
+void console_printtext(const uint8_t * what) { }
+void console_printpgm_P(PGM_P what) { }
+void console_printhex8(uint8_t what) { }
+void console_printdec(uint8_t what) { }
+
+#endif /* SERIALCONSOLE */
